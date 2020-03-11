50898ac3ba49bcf86fd5eb8192ec74bd6d242228
Jojitoon <jojitoon@gmail.com>
2020-03-11 10:21:11 +0100

    careful

--- index.js
+++ index.js
@@ -1,13 +1,9 @@
-
-
-import {AppRegistry} from 'react-native';
+import { AppRegistry } from 'react-native';
 import App from './App';
-import {name as appName} from './app.json';
+import { name as appName } from './app.json';
 import initApp from './initApp';
 import store from './src/store';
 
 initApp(store);
 
-
-
-AppRegistry.registerComponent(appName, () => x);
+AppRegistry.registerComponent(appName, () => App);
